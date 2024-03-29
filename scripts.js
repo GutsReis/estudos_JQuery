4b004912784afaf6a0ce132c25073ea92b6f20de
Gustavo Reis <reisgg9@gmail.com>
2024-03-28 23:23:55 -0300

    alteração para fazer funcionar com teste em branch

--- scripts.js
+++ scripts.js
@@ -7,21 +7,21 @@ $(document).ready(function() {
     $('#botao-cancelar').click(function(){
         $('form').slideUp()
     })
-// slideDown e slideUp sendo utilizados como animação de mostrar e guardar o formulário
+    
     $('form').on('submit', function(e) {
         e.preventDefault();
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display:none;></li>');
+        const novoItem = $('<li style="display: none;></li>');
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem)
         $(`
         <div class="overlay-imagem-link'>
-                <a href="${enderecoDaNovaImagem}" target="_blank title ="Ver imagem em tamanho real"
-                    ver imagem em tamanho real
-                </a>
+            <a href="${enderecoDaNovaImagem}" target="_blank" title ="Ver imagem em tamanho real"
+                Ver imagem em tamanho real
+            </a>
             </div>
         `).appendTo(novoItem)
         $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn();
+        $(novoItem).fadeIn()
         $('endereco-imagem-nova').val('')
     })
 })
